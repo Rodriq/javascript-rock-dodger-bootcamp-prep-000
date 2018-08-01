37942653c1aaca6cc1003b6b111da4d8f4662252
Fogang Rodriq <fogangrodriq@gmail.com>
2018-08-01 04:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ window.requestAnimationFrame(moveRock)
  */
  function endGame() {
  	clearInterval(gameInterval);
- 	for each (var rock in ROCKS){
+ 	forEach (rock in ROCKS){
  	  rock.remove()
  	}
  	// ROCKS.splice(0, ROCKS.length);
